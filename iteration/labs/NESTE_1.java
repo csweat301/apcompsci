d0aa71805bf20bad6a58549c9c547495bf626538
csweat301 <60934053+csweat301@users.noreply.github.com>
2020-02-13 00:42:37 -0500

    Added '4.5 - Iteration' Labs

---
+++ iteration/labs/NESTE_1.java
@@ -0,0 +1,30 @@
+package labs;
+
+import java.util.Scanner;
+
+public class NESTE_1 {
+
+	public void go() {
+		System.out.println("Input values in the following format: {NUMB} {CHAR}");
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			try {
+				int count = Integer.valueOf(line.substring(0, line.indexOf(" ")));
+				String value = line.substring(line.indexOf(" ") + 1);
+
+				for (int i = 0; i <= count; i++) {
+					for (int i2 = 0; i2 < i; i2++)
+						System.out.print(value);
+					System.out.println();
+				}
+			} catch (Exception ex) {
+				System.out.println("Error in input, please try again in the following format: {NUMB} {CHAR}");
+			}
+		}
+
+		scanner.close();
+	}
+	
+}
---
+++ iteration/labs/NESTE_3.java
@@ -0,0 +1,32 @@
+package labs;
+
+import java.util.Scanner;
+
+public class NESTE_3 {
+
+	public void go() {
+		System.out.println("Input values in the following format: {NUMB} {CHAR}");
+		Scanner scanner = new Scanner(System.in);
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			try {
+				int count = Integer.valueOf(line.substring(0, line.indexOf(" ")));
+				String value = line.substring(line.indexOf(" ") + 1);
+
+				for (int i = 0; i <= count; i++) {
+					for (int i2 = count; i2 > i; i2--)
+						System.out.print(" ");
+					for (int i2 = 0; i2 < i; i2++)
+						System.out.print(value);
+					System.out.println();
+				}
+			} catch (Exception ex) {
+				System.out.println("Error in input, please try again in the following format: {NUMB} {CHAR}");
+			}
+		}
+
+		scanner.close();
+	}
+	
+}
